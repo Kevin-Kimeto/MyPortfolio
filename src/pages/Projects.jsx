810cc9c0df891619bb9490368bbe89722edd3767
Kevin Kimeto <m.officialkev@gmail.com>
2022-10-17 12:23:56 +0300

    Fixed contact section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,7 +4,7 @@ import { ProjectList } from '../helpers/ProjectList';
 
 const Projects = () => {
   return (
-    <div className='flex flex-col justify-center items-center mt-14 pt-4 w-full h-auto bg-gray-500'>
+    <div className='flex flex-col justify-center items-center mt-14 pt-4 w-full h-auto pb-2 bg-gray-500'>
       <h1 className='text-4xl font-bold border-b-4 text-white border-gray-200 p-2 inline'>My Projects</h1>
       <h4 className='text-center md:mt-8 mt-3 text-2xl text-gray-300'>These are some of my projects:</h4>
       <div className='grid md:grid-cols-3 grid-cols-1 justify-center place-items-center md:mt-7 mt-1 md:mb-16 mb-3'>
